15593966f113b3f968f0f9a8e03d3261bd9030e2
petr791 <fisherman003210gmail.com>
2022-06-30 14:54:13 +0300

    next

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
 const anims = document.querySelectorAll('.anim');
-console.log('gg');
-
-
 
 document.addEventListener('DOMContentLoaded', function() {
 
 
-
-    // Ваш скрипт
     function animblock() {
         for (let index = 0; index < anims.length; index++) {
             const anim = anims[index];
