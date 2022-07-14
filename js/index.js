e90243d299586210fe4f4e1fd3adb4ff15f56bd1
petr791 <fisherman003210gmail.com>
2022-07-14 11:38:51 +0300

    next

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-const anims = document.querySelectorAll('.anim');
+ const anims = document.querySelectorAll('.anim');
 
-document.addEventListener('DOMContentLoaded', function() {
+ document.addEventListener('DOMContentLoaded', function() {
 
 
-    function animblock() {
-        if (window.innerWidth >= 768) {
-            for (let index = 0; index < anims.length; index++) {
-                const anim = anims[index];
+     function animblock() {
+         if (window.innerWidth >= 768) {
+             for (let index = 0; index < anims.length; index++) {
+                 const anim = anims[index];
 
-                anim.classList.add('_active')
-            }
-        }
-    }
-    setTimeout(animblock, 500);
-}, false);
+                 anim.classList.add('_active')
+             }
+         }
+     }
+     setTimeout(animblock, 500);
+ }, false);
